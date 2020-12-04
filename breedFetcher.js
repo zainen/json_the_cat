18e279a87929c629eb85404b875ebede2e9cf99b
zainen <zainensuzuki@gmail.com>
2020-12-04 00:37:34 +0000

    finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,35 @@
 const request = require('request');
 let URL = 'https://api.thecatapi.com/v1/breeds/search';
-const name = process.argv[2];
 
 
-const breedFetcher = (name) => {
-  URL += `?q=${name}`;
+// const breedFetcher = (name) => {
+//   URL += `?q=${name}`;
+//   request(URL, (err, response, body) => {
+//     if (err) {
+//       return console.log('URL Error: ', err);
+//     }
+//     const arr = JSON.parse(body);
+//     if (arr.length === 0) {
+//       return console.log('Breed not found');
+//     }
+//     console.log(arr[0].description);
+//   });
+// };
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  URL += `?q=${breedName}`;
   request(URL, (err, response, body) => {
-    if (err) return console.log('URL Error: ', err);
+    if (err) {
+      callback(err, null);
+      return;
+    }
     const arr = JSON.parse(body);
-    if (arr.length === 0) return console.log('Breed not found');
-    console.log(arr[0].description);
+    if (arr.length === 0) {
+      callback(`Breed ${breedName} not found`, null);
+      return;
+    }
+    callback(null, arr[0].description);
   });
 };
-breedFetcher(name);
+module.exports = { fetchBreedDescription };
